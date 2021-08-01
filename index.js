28d8feda2923588d1c67eb014cb04281520c98e6
karan rawat <rawatkaran181@gmail.com>
2021-08-01 20:31:16 +0530

    lllmlml

---
+++ index.js
@@ -0,0 +1,41 @@
+
+// function statement
+
+function abc(){
+  console.log("a called");
+};
+ 
+
+// function expression
+var b = function(params){
+
+     console.log(params);
+
+};
+
+abc();
+function xyz(){
+     
+};
+
+b(xyz);
+
+//_________________________ Anonymous function
+
+// const c =function() {
+
+
+// }
+
+//__________________Named function expression
+
+// var b = function xyz(){
+
+//     console.log("b is called");
+
+// };
+
+
+// first class function
+
+ 
